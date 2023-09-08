60bf3061194c7094c5966f84c43429481f9bb5b7
lebrancconvas <lebrancconvas@gmail.com>
2023-09-08 22:32:39 +0700

    🎉 feat(init): init workshop

---
+++ hello/target/debug/deps/hello-71085f0b4b4be228.d
@@ -0,0 +1,5 @@
+/Users/poomyimyuean/Desktop/Poomkun/Hub/Code/Course/programming-language/rust/book/basic/hello/target/debug/deps/hello-71085f0b4b4be228.rmeta: src/main.rs
+
+/Users/poomyimyuean/Desktop/Poomkun/Hub/Code/Course/programming-language/rust/book/basic/hello/target/debug/deps/hello-71085f0b4b4be228.d: src/main.rs
+
+src/main.rs:
---
+++ hello/target/debug/deps/hello-86001d38201fef28.d
@@ -0,0 +1,5 @@
+/Users/poomyimyuean/Desktop/Poomkun/Hub/Code/Course/programming-language/rust/book/basic/hello/target/debug/deps/hello-86001d38201fef28.rmeta: src/main.rs
+
+/Users/poomyimyuean/Desktop/Poomkun/Hub/Code/Course/programming-language/rust/book/basic/hello/target/debug/deps/hello-86001d38201fef28.d: src/main.rs
+
+src/main.rs:
